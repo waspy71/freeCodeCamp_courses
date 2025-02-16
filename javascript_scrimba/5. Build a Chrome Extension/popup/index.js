7135e3c417aae5df8fadc49c623cdfc1adc4f5f7
wasp <wasp123321@gmail.com>
2025-02-16 18:20:34 +0100

    Build an extension part 52/54

--- javascript_scrimba/5. Build a Chrome Extension/popup/index.js
+++ javascript_scrimba/5. Build a Chrome Extension/popup/index.js
@@ -4,33 +4,52 @@ let myLeads = localStorage.getItem('myLeads') ? JSON.parse(localStorage.getItem(
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 
 if(myLeads) {
-  renderLeads()
+  render(myLeads)
+}
+
+function render(leads) {
+  let listItems = ''
+  for( let i = 0; i < leads.length; i++) {
+    // ulEl.innerHTML += '<li>' + leads[i] + '</li>'
+    // let liEl = document.createElement('li')
+    // liEl.textContent = leads[i]
+    // ulEl.appendChild(liEl)
+    listItems += `<li><a target='_blank' href="${leads[i]}">${leads[i]}</a></li>`
+  }
+  
+  ulEl.innerHTML = listItems
 }
 
 // let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 // console.log('leadsFromLocalStorage', leadsFromLocalStorage)
 
+tabBtn.addEventListener('click', () => {
+  browser.tabs.query({ active: true, currentWindow: true })
+    .then(tabs => {
+      myLeads.push(tabs[0].url)
+      localStorage.setItem('myLeads', JSON.stringify(myLeads))
+      render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener('dblclick', () => {
+  localStorage.clear()
+  myLeads = []
+  // ulEl.innerHTML = ''
+  render(myLeads)
+})
+
 inputBtn.addEventListener('click', () => {
   if(inputEl.value) {
     myLeads.push(inputEl.value)
     inputEl.value = ''
-    renderLeads()
+    render(myLeads)
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
     console.log('clicked from event listener', localStorage.getItem('myLeads'))
   }
 })
 
-function renderLeads() {
-  let listItems = ''
-  for( let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += '<li>' + myLeads[i] + '</li>'
-    // let liEl = document.createElement('li')
-    // liEl.textContent = myLeads[i]
-    // ulEl.appendChild(liEl)
-    listItems += `<li><a target='_blank' href="${myLeads[i]}">${myLeads[i]}</a></li>`
-  }
-  
-  ulEl.innerHTML = listItems
-}
